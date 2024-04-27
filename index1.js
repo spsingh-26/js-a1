202dfc696d468ac0ce37c9e6b3eb2cc0b04ebba7
spsingh-26 <151997398+spsingh-26@users.noreply.github.com>
2024-04-27 16:20:36 +0530

    Add files via upload

---
+++ index1.js
@@ -0,0 +1,45 @@
+/***Q1. Add two numbers */
+let ab = 10;
+let bc = 15;
+console.log(ab + bc)
+
+
+/***Q2. Find if the conditions are obeyed or not */
+let A = 5
+let B = 3
+console.log(A<10<10 && A>B>B) //false
+console.log(A<10<10) //false
+console.log(A>B>B) //false
+
+
+/***Q3 Check the conditions */
+const a = 2
+const b = 5
+console.log(a/10 && b/10 ? "True" : "False")
+console.log(a/10 && b/10 ? "True" : "False")
+console.log(a/10 || b/10 ? "True" : "False")
+
+
+/****Q4 Find the firstw digit of a 4 digit number */
+
+
+/****Q5 Find the last digit of a 4 digit number */
+
+
+/****Q6 Find teh remainder */
+let AA = 2;
+let BB = 9;
+console.log(AA%BB)
+
+/*****Q7 Multiply two numbers */
+let x = 2;
+let y = 5;
+console.log(x*y)
+
+/***Q8 Marks Calculator */
+let eng = 50
+let math = 20
+let science = 100
+sum = eng+math+science
+console.log("Sum " + sum);
+console.log("Average " + sum/3)
